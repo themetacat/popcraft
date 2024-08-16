d016bb2b878eda70787dedcd2db1178bb508ebd9
MJumpKing <2572430586@qq.com>
2024-08-16 18:13:51 +0800

    pixelaw & popcraft redeploy

--- start.sh
+++ start.sh
@@ -48,14 +48,14 @@ fi
 # forge script script/CallSystemExtension.s.sol --rpc-url $RPC_URL --broadcast
 # sleep 1
 
-echo -e "Register default app paint to World contract."
-cd ../paint
-forge script script/PaintExtension.s.sol --rpc-url $RPC_URL --broadcast
-sleep 1
-PRIVATE_KEY=$(grep -E "^PRIVATE_KEY=" .env | cut -d '=' -f2-)
-PRIVATE_KEY=${PRIVATE_KEY:-'0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6'}
-cast send $WORLD_ADDRESS --rpc-url $RPC_URL --private-key $PRIVATE_KEY "paint_PaintSystem_init()" ""
-sleep 1
+# echo -e "Register default app paint to World contract."
+# cd ../paint
+# forge script script/PaintExtension.s.sol --rpc-url $RPC_URL --broadcast
+# sleep 1
+# PRIVATE_KEY=$(grep -E "^PRIVATE_KEY=" .env | cut -d '=' -f2-)
+# PRIVATE_KEY=${PRIVATE_KEY:-'0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6'}
+# cast send $WORLD_ADDRESS --rpc-url $RPC_URL --private-key $PRIVATE_KEY "paint_PaintSystem_init()" ""
+# sleep 1
 
 # echo -e "Register default app snake to World contract."
 # cd ../snake
