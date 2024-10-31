58ba0cab395c37c43c7394986bca963187dbda13
MJumpKing <2572430586@qq.com>
2024-10-31 09:48:25 +0800

    redeploy contracts and update overtime

--- packages/client/src/components/BoxPrompt/index.tsx
+++ packages/client/src/components/BoxPrompt/index.tsx
@@ -40,7 +40,7 @@ export default function BoxPrompt({ coordinates, timeControl, playFun, handleEoa
     network: { palyerAddress },
     systemCalls: { interact, payFunction, registerDelegation },
   } = useMUD();
-  const overTime = 243;
+  const overTime = 92;
   const [timeLeft, setTimeLeft] = useState(overTime);
   const [warnBox, setWarnBox] = useState(false);
   const [dataq, setdataq] = useState(false);
