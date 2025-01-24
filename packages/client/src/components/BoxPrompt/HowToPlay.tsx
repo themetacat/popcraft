45c3eb46d4e87e753a9c723e3e7161904a17f437
MJumpKing <2572430586@qq.com>
2025-01-24 17:11:47 +0800

    style: how to play & add B3 rewards img

--- packages/client/src/components/BoxPrompt/HowToPlay.tsx
+++ packages/client/src/components/BoxPrompt/HowToPlay.tsx
@@ -7,31 +7,65 @@ import PlayerGuide3 from '../../images/HowToPlay/PlayerGuide_3.webp';
 import PlayerGuide4 from '../../images/HowToPlay/PlayerGuide_4.webp';
 import PlayerGuide5 from '../../images/HowToPlay/PlayerGuide_5.webp';
 import RewardsMorph from '../../images/HowToPlay/RewardsMorph.webp';
+import RewardsB3 from '../../images/HowToPlay/RewardsB3.webp';
 import MyPlantsGuide from '../../images/HowToPlay/MyPlantsGuide.webp';
 import close from "../../images/Plants/close.webp";
+import { useTopUp } from "../select";
 
 interface Props {
   setShowHowToPlay: any
 }
 
 export default function HowToPlay({ setShowHowToPlay }: Props) {
-  const images = [PlayerGuide1, PlayerGuide2, PlayerGuide3, PlayerGuide4, PlayerGuide5, RewardsMorph, MyPlantsGuide];
+  const { chainId } = useTopUp();
+  let rewardsImg = RewardsMorph;
+  if(chainId === 8333){
+    rewardsImg = RewardsB3;
+  }
+  const images = [PlayerGuide1, PlayerGuide2, PlayerGuide3, PlayerGuide4, PlayerGuide5, rewardsImg, MyPlantsGuide];
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 2000); // 每2秒切换一次
-    return () => clearInterval(interval);
-  }, [images.length]);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  //   }, 2000); // 每2秒切换一次
+  //   return () => clearInterval(interval);
+  // }, [images.length]);
+
+  const switchPage = (page = 1) => {
+    if (page < 0) {
+      page = images.length - 1
+    } else if (page > images.length - 1) {
+      page = 0
+    }
+    setCurrentIndex(page);
+  };
 
   return (
     <div className={style.overlayBuy}>
       <div className={style.content}>
-        <div className={howToPlayStyle.cornerImage} onClick={() => { setShowHowToPlay(false); }}>
+        <div className={howToPlayStyle.leftArrow} onClick={() => { switchPage(currentIndex - 1) }}>
+          &lt;
+        </div>
+        <div className={howToPlayStyle.cornerImage} onClick={() => { setShowHowToPlay(false) }}>
           <img src={close} />
         </div>
         <img src={images[currentIndex]} alt={`Guide ${currentIndex + 1}`} className={howToPlayStyle.carouselImage} />
+        <div className={howToPlayStyle.rightArrow} onClick={() => { switchPage(currentIndex + 1) }}>
+          &gt;
+        </div>
+        <div className={howToPlayStyle.navigation}>
+          {images.map((_, index) => (
+            <span
+              key={index}
+              className={`${howToPlayStyle.navNumber} ${index === currentIndex ? howToPlayStyle.active : ''}`}
+              onClick={() => setCurrentIndex(index)}
+            >
+              {index + 1}
+            </span>
+          ))}
+        </div>
+
       </div>
     </div>
   )
