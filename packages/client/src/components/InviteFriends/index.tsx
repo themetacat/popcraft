790082e72126bfe4728da994a81b5a56b6a34509
k1ic <k1ic@k1icdeMacBook-Air.local>
2025-03-27 14:43:42 +0800

    update Invitation PC style

--- packages/client/src/components/InviteFriends/index.tsx
+++ packages/client/src/components/InviteFriends/index.tsx
@@ -26,27 +26,52 @@ export default function InviteFriends({ isMobile }: InviteProps) {
 
     const [isCopyBtnClicked, setIsCopyBtnClicked] = useState(false);
     const handleCopyBtnClick = (InviteCode) => {
+        setIsCopyBtnClicked(true);
+        setTimeout(() => {
+            setIsCopyBtnClicked(false);
+        }, 1000);
+
         navigator.clipboard.writeText(InviteCode).then(
             function () {
                 setToastMsg("Invite Code Copied!");
                 setShowSuccessModal(true);
                 setTimeout(() => {
                     setShowSuccessModal(false);
-                }, 2000);
+                }, 1000);
             },
             function (err) {
-                setToastMsg("Failed to copy the invite code. Please try again!");
+                setToastMsg("Copy failed, retry!");
                 setShowErrorToast(true);
                 setTimeout(() => {
                     setShowErrorToast(false);
-                }, 3000);
+                }, 2000);
             }
         );
     };
 
     const [isTwitterBtnClicked, setIsTwitterBtnClicked] = useState(false);
-    const handleTwitterBtnClick = () => {
-        setIsTwitterBtnClicked(!isTwitterBtnClicked);
+    const handleTwitterBtnClick = (InviteCode) => {
+        setIsTwitterBtnClicked(true);
+        setTimeout(() => {
+            setIsTwitterBtnClicked(false);
+        }, 1000);
+
+        navigator.clipboard.writeText(InviteCode).then(
+            function () {
+                setToastMsg("Invite Code Copied!");
+                setShowSuccessModal(true);
+                setTimeout(() => {
+                    setShowSuccessModal(false);
+                }, 1000);
+            },
+            function (err) {
+                setToastMsg("Share failed, retry!");
+                setShowErrorToast(true);
+                setTimeout(() => {
+                    setShowErrorToast(false);
+                }, 2000);
+            }
+        );
     };
 
     if (!isMobile) {
@@ -57,7 +82,7 @@ export default function InviteFriends({ isMobile }: InviteProps) {
                     <button>Invite</button>
                 </div>
 
-                {isShowInviteModal && (
+                {true && (
                     <div className={style.overlay}>
                         <div className={style.modalContainer}>
                             <div className={style.title}>
@@ -73,37 +98,44 @@ export default function InviteFriends({ isMobile }: InviteProps) {
                             />
                             <div className={style.contentsWrap}>
                                 <div className={style.contentsTitle}>Your Invitation Code</div>
-                                <div className={style.inviteSection}>
-                                    <span className={style.inviteCode}>{InviteCode}</span>
-                                    <div className={style.copyInviteContainer}>
-                                        <button
-                                            className={style.copyInviteBtn}
-                                            onClick={() => {
-                                                handleCopyBtnClick(InviteCode);
-                                            }}
-                                        >
-                                            <img
-                                                src={isCopyBtnClicked ? CopyBtnClickImg : CopyBtnImg}
-                                                alt="Copy Invitation Code"
-                                            />
-                                        </button>
-                                        <span className={style.copyInviteWord}>Copy Link</span>
+                                <div className={style.middlePart}>
+                                    <div className={style.inviteSection}>
+                                        <span className={style.inviteCode}>{InviteCode}</span>
+                                        <div className={style.shareInviteWarp}>
+                                            <div className={style.copyInviteContainer}>
+                                                <button
+                                                    className={style.copyInviteBtn}
+                                                    onClick={() => {
+                                                        handleCopyBtnClick(InviteCode);
+                                                    }}
+                                                >
+                                                    <img
+                                                        src={isCopyBtnClicked ? CopyBtnClickImg : CopyBtnImg}
+                                                        alt="Copy Invitation Code"
+                                                    />
+                                                    <span className={style.copyInviteWord}>Copy Link</span>
+                                                </button>
+                                            </div>
+                                            <div className={style.inviteShareXContainer}>
+                                                <button
+                                                    className={style.shareXBtn}
+                                                    onClick={() => {
+                                                        handleTwitterBtnClick(InviteCode);
+                                                    }}
+                                                >
+                                                    <img
+                                                        src={isTwitterBtnClicked ? TwitterClickImg : TwitterImg}
+                                                        alt="Invitation Code Share Twitter"
+                                                    />
+                                                </button>
+                                                <span className={style.shareXInviteWord}>Share X(Twitter)</span>
+                                            </div>
+                                        </div>
+                                        <p className={style.inviteNote}>Each chain's invitation is a separate link and calculated individually.</p>
                                     </div>
-
-                                    {/* <div className={style.inviteShareContainer}>
-                                        <button className={style.btnShare} onClick={handleTwitterBtnClick}>
-                                        <img 
-                                                src={isTwitterBtnClicked ? TwitterClickImg : TwitterImg}
-                                                alt="Copy Invitation Code"
-                                            />
-                                        </button>
-                                        <span>Share X(Twitter)</span>
-                                    </div> */}
+                                    <p className={style.inviteRule}>You'll get 10% of your friends' scores from the project team.</p>
                                 </div>
-                                <p className={style.note}>Each chain's invitation is a separate link and calculated individually.</p>
-                                <div className={style.infoBox}>
-                                    <p>You'll get 10% of your friends' scores from the project team.</p>
-                                </div>
+                                <div className={style.dividingLine}></div>
                             </div>
                         </div>
 
@@ -113,12 +145,8 @@ export default function InviteFriends({ isMobile }: InviteProps) {
                                 <p className={style.copyToastColor}>{toastMsg}</p>
                             </div>
                         )}
-
                     </div>
                 )}
-
-
-
             </>
         );
     } else {
