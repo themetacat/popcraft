4913d0fac04717aa90429fa9b7c16475a56f7b6d
MJumpKing <2572430586@qq.com>
2025-04-28 10:34:25 +0800

    update: targe mode 1 -> 500 scores

--- packages/client/src/constant/index.ts
+++ packages/client/src/constant/index.ts
@@ -2,6 +2,6 @@
 export const MODE_SCORE_CHAL_SUCCESS_SCORE = 500n;
 export const MODE_TARGE: { [key: number]: string } = {
     0: "Clear Board",
-    1: "Score Challenge",
+    1: "500 Scores",
   };
 export const OVER_TIME = 122;
