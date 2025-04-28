3c2e9d9285f73744e6c4bc5af0522bcc1cb3f9d4
k1ic <k1ic@k1icdeMacBook-Air.local>
2025-04-28 14:10:24 +0800

    Change to uppercase

--- packages/client/src/constant/index.ts
+++ packages/client/src/constant/index.ts
@@ -2,6 +2,6 @@
 export const MODE_SCORE_CHAL_SUCCESS_SCORE = 500n;
 export const MODE_TARGE: { [key: number]: string } = {
     0: "Clear Board",
-    1: "500 Scores",
+    1: "500 SCORES",
   };
 export const OVER_TIME = 122;
